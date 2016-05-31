b3e483917ca32e43e1ef10e5356cef881c0df171
cruzpa <cruzpa95@gmail.com>
2016-05-31 14:43:57 -0300

    Agregue equipo
    Crea un equipo de algoformers recibiendo una lista de Strings

---
+++ src/fiuba/algo3/algoformers/src/Equipo.java
@@ -0,0 +1,26 @@
+package fiuba.algo3.algoformers.modelo;
+
+import java.util.ArrayList;
+
+public class Equipo {
+	//EL CAPITAN SIEMPRE VA A SER EL PRIMERO DE LA LISTA.
+	private ArrayList<AlgoFormer> listaAlgoFormer;
+	
+	/*	private AlgoFormer capitan;
+	private AlgoFormer subordinadoA;
+	private AlgoFormer subordinadoB;
+	
+	public Equipo(String archivoCapitan, String archivoSubordinadoA, String archivoSubordinadoB){
+		//Equipo le pasa los nombres de archivo al constructor de AlgoFormer
+		capitan = new AlgoFormer(archivoCapitan);
+		subordinadoA = new AlgoFormer(archivoSubordinadoA);
+		subordinadoB = new AlgoFormer(archivoSubordinadoB);	
+	}*/
+	public Equipo(ArrayList<String> listaArchivos){
+		for(String archivo : listaArchivos)
+			listaAlgoFormer.add(new AlgoFormer(archivo));
+	}
+	public AlgoFormer getCapitan(){
+		return listaAlgoFormer.get(0);
+	}
+}
