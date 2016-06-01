91cea84c24777257dea449586225de9edfb6e6b2
pabloernesto <caussabon@gmail.com>
2016-06-01 16:43:54 -0300

    Completado el método colocarChispaSuprema

--- src/fiuba/algo3/algoformers/src/Celda.java
+++ src/fiuba/algo3/algoformers/src/Celda.java
@@ -2,20 +2,10 @@ package fiuba.algo3.algoformers.modelo;
 
 public class Celda {
 
-	/* Todo lo que esta comentado por el momento no se necesita, pero ya que estaba lo deje escrito*/
+	private AlgoFormer algoformer = null;
+	private boolean contieneChispaSuprema = false;
 	
-	
-	/*private Superficie supAire;
-	private Superficie supTierra;
-	private Bonus bonus;*/
-	private AlgoFormer algoformer;
-	
-	public Celda(/*Superficie supAire, Superficie supTierra*/){
-		/*this.supAire = supAire;
-		this.supTierra = supTierra;
-		this.bonus = null;*/
-		this.algoformer = null;
-	}
+	public Celda() {}
 	
 	public void setAlgoformer(AlgoFormer algoformer){
 		this.algoformer = algoformer;
@@ -46,38 +36,11 @@ public class Celda {
     
     public void colocarChispaSuprema()
     {
-        ;
+        contieneChispaSuprema = true;
     }
     
     public boolean contieneChispaSuprema()
     {
-        return false;
+        return contieneChispaSuprema;
     }
-	
-/*
-	public void setBonus(Bonus bonus){
-		this.setBonus = bonus;
-	}
-	
-	public Bonus getBonus(){
-		return this.bonus;
-	}
-	
-	public void eliminarBonus(){
-		this.bonus = null;
-	}
-	
-	public Efecto getEfecto(UnidadTerrestre algoformer){
-		
-	}
-	
-	public Efecto getEfecto(UnidadHumanoide algoformer){
-		
-	}
-	
-	public Efecto getEfecto(UnidadAerea algoformer){
-		
-	}
-	
-	*/
 }
