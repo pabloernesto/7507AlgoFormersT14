23bf38cd8733fea5c5b0da79bc2b90a4e77b0e1b
pabloernesto <caussabon@gmail.com>
2016-06-14 14:31:02 -0300

    Nueva prueba
    testAtacarHaceQueElAlgoFormerActualAtaque

--- test/fiuba/algo3/algoformers/test/JugadorTest.java
+++ test/fiuba/algo3/algoformers/test/JugadorTest.java
@@ -5,6 +5,9 @@ import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
 
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
 import fiuba.algo3.algoformers.juego.Jugador;
 import fiuba.algo3.algoformers.factories.AutoBotFactory;
 import fiuba.algo3.algoformers.algoformers.AlgoFormer;
@@ -14,8 +17,9 @@ import java.lang.NullPointerException;
 
 public class JugadorTest
 {
-    Jugador jugador;
-
+    @Mock protected AlgoFormer algoformerActual;
+    @InjectMocks Jugador jugador;
+    
     @Before
     public void setUp()
     {
@@ -25,7 +29,22 @@ public class JugadorTest
     @Test(expected = NullPointerException.class)
     public void testAtacarSinElegirAlgoFormerCausaExcepcion()
     {
+        jugador = new Jugador(new AutoBotFactory());
+
         List<AlgoFormer> equipo = jugador.getListaAlgoformers();
         jugador.atacar(equipo.get(0));
 	}
+	
+	@Test
+    public void testAtacarHaceQueElAlgoFormerActualAtaque()
+    {
+        jugador.elegirAlgoFormer("Optimus Prime");
+        MockitoAnnotations.initMocks(this);
+        
+        AlgoFormer objetivo = jugador.getListaAlgoformers().get(0);
+        jugador.atacar(objetivo);
+        
+        verify(algoformerActual).atacar(objetivo);
+	}
 }
+
