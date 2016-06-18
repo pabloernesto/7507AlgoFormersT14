e18665db576ab24c26e4d9c5c0e06c29a40081d1
Farfanatico <niko.f2@gmail.com>
2016-06-18 01:33:04 -0300

    Agrego el handler Informacion
    INFORMACION

---
+++ src/fiuba/algo3/algoformers/vista/eventos/OpcionInformacionEventHandler.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.algoformers.vista.eventos;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+public class OpcionInformacionEventHandler implements EventHandler<ActionEvent> {
+
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+       
+    }
+}
