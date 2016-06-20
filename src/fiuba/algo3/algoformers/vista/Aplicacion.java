7cc6cbfba3cb1074a2e0da4643ab9ed3068526ad
pabloernesto <caussabon@gmail.com>
2016-06-20 06:49:54 -0300

    Cambio cosmetico
    Eliminado nivel extra de indentacion

--- src/fiuba/algo3/algoformers/vista/Aplicacion.java
+++ src/fiuba/algo3/algoformers/vista/Aplicacion.java
@@ -6,39 +6,38 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-	public class Aplicacion extends Application {
+public class Aplicacion extends Application {
 
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	    @Override
-	    public void start(final Stage stage) throws Exception {
-	    	
-	    	Juego juego = new Juego();
-	        stage.setTitle("Juego Algoformers");
-	        ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage, juego);
-	        Scene escenaJuego = new Scene(contenedorPrincipal, 640, 480);
-	        
-	        AplicacionOnKeyPressEventHandler AplicacionOnKeyPressEventHandler = new AplicacionOnKeyPressEventHandler(stage, contenedorPrincipal.getBarraDeMenu());
-	        escenaJuego.setOnKeyPressed(AplicacionOnKeyPressEventHandler);
-	        
-	        ContenedorEleccionJugador contenedorEleccion = new ContenedorEleccionJugador(stage, escenaJuego, juego, contenedorPrincipal);
-	        Scene escenaEleccion = new Scene(contenedorEleccion, 640, 480);
-	        
-	        ContenedorReglas contenedorReglas = new ContenedorReglas(stage, escenaEleccion);
-	        Scene escenaReglas = new Scene(contenedorReglas, 640, 480);
-	        
-	        Bienvenida contenedorBienvenidos = new Bienvenida(stage, escenaReglas);
-	        Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 640, 480);
+    @Override
+    public void start(final Stage stage) throws Exception {
+    	
+    	Juego juego = new Juego();
+        stage.setTitle("Juego Algoformers");
+        ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage, juego);
+        Scene escenaJuego = new Scene(contenedorPrincipal, 640, 480);
+        
+        AplicacionOnKeyPressEventHandler AplicacionOnKeyPressEventHandler = new AplicacionOnKeyPressEventHandler(stage, contenedorPrincipal.getBarraDeMenu());
+        escenaJuego.setOnKeyPressed(AplicacionOnKeyPressEventHandler);
+        
+        ContenedorEleccionJugador contenedorEleccion = new ContenedorEleccionJugador(stage, escenaJuego, juego, contenedorPrincipal);
+        Scene escenaEleccion = new Scene(contenedorEleccion, 640, 480);
+        
+        ContenedorReglas contenedorReglas = new ContenedorReglas(stage, escenaEleccion);
+        Scene escenaReglas = new Scene(contenedorReglas, 640, 480);
+        
+        Bienvenida contenedorBienvenidos = new Bienvenida(stage, escenaReglas);
+        Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 640, 480);
 
 
-	        stage.setScene(escenaBienvenidos);
-	        stage.setFullScreen(true);
-	        stage.setFullScreenExitHint("");
+        stage.setScene(escenaBienvenidos);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
 
-	        stage.show();
+        stage.show();
 
-	    }
-
+    }
 }
