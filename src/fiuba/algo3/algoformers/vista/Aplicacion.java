b236c56d45dbf516a1b629f052bbb91f95dc6cba
Farfanatico <niko.f2@gmail.com>
2016-06-20 16:38:21 -0300

    Agregado tablero
    Tablero

--- src/fiuba/algo3/algoformers/vista/Aplicacion.java
+++ src/fiuba/algo3/algoformers/vista/Aplicacion.java
@@ -1,53 +1,54 @@
 package fiuba.algo3.algoformers.vista;
 
+import java.io.File;
+
+import javax.sound.sampled.AudioFormat;
+
+import fiuba.algo3.algoformers.escenario.Tablero;
 import fiuba.algo3.algoformers.juego.Juego;
 import fiuba.algo3.algoformers.vista.eventos.AplicacionOnKeyPressEventHandler;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class Aplicacion extends Application {
+	public class Aplicacion extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
 
-    @Override
-    public void start(final Stage stage) throws Exception {
-    	
-    	Juego juego = new Juego();
-        stage.setTitle("Juego Algoformers");
-        ContenedorPrincipal contenedorPrincipal =
-        	new ContenedorPrincipal(stage, juego);
-        Scene escenaJuego = new Scene(contenedorPrincipal, 640, 480);
-        
-        AplicacionOnKeyPressEventHandler AplicacionOnKeyPressEventHandler =
-        	new AplicacionOnKeyPressEventHandler(
-        		stage,
-        		contenedorPrincipal.getBarraDeMenu());
-        escenaJuego.setOnKeyPressed(AplicacionOnKeyPressEventHandler);
-        
-        ContenedorEleccionJugador contenedorEleccion =
-        	new ContenedorEleccionJugador(
-        		stage, 
-        		escenaJuego,
-        		juego,
-        		contenedorPrincipal);
-        Scene escenaEleccion = new Scene(contenedorEleccion, 640, 480);
-        
-        ContenedorReglas contenedorReglas =
-        	new ContenedorReglas(stage, escenaEleccion);
-        Scene escenaReglas = new Scene(contenedorReglas, 640, 480);
-        
-        Bienvenida contenedorBienvenidos = new Bienvenida(stage, escenaReglas);
-        Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 640, 480);
+	    @Override
+	    public void start(final Stage stage) throws Exception {
+	    	
 
+	    	
+	    	Juego juego = new Juego();
+	    	Tablero tablero = Tablero.getInstance();
+	        stage.setTitle("Juego Algoformers");
+	        ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage,tablero); //, algoformer);
+	        Scene escenaJuego = new Scene(contenedorPrincipal, 640, 480);
+	        
+	        AplicacionOnKeyPressEventHandler AplicacionOnKeyPressEventHandler = new AplicacionOnKeyPressEventHandler(stage, contenedorPrincipal.getBarraDeMenu());
+	        escenaJuego.setOnKeyPressed(AplicacionOnKeyPressEventHandler);
+	        
+	        ContenedorEleccionJugador contenedorEleccion = new ContenedorEleccionJugador(stage, escenaJuego, juego);
+	        Scene escenaEleccion = new Scene(contenedorEleccion, 640, 480);
+	        
+	        ContenedorReglas contenedorReglas = new ContenedorReglas(stage, escenaEleccion);
+	        Scene escenaReglas = new Scene(contenedorReglas, 640, 480);
+	        
+	        Bienvenida contenedorBienvenidos = new Bienvenida(stage, escenaReglas);
+	        Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 640, 480);
+	        
+	        stage.setScene(escenaBienvenidos);
+	        stage.setFullScreen(true);
+	        stage.setFullScreenExitHint("");
 
-        stage.setScene(escenaBienvenidos);
-        stage.setFullScreen(true);
-        stage.setFullScreenExitHint("");
+	        stage.show();
 
-        stage.show();
-    }
+	    }
+
 }
-
