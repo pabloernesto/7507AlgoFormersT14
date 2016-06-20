05a43170e21a2934fed83b247a8b0b4a4042f2ca
Farfanatico <niko.f2@gmail.com>
2016-06-20 18:23:09 -0300

    Removidos warnings
    Arregle para que no tire mas warnings a medida que usen cosas
    agreguenlas

--- src/fiuba/algo3/algoformers/vista/BarraDeMenu.java
+++ src/fiuba/algo3/algoformers/vista/BarraDeMenu.java
@@ -9,6 +9,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 public class BarraDeMenu extends MenuBar {
@@ -16,8 +17,7 @@ public class BarraDeMenu extends MenuBar {
     MenuItem opcionPantallaCompleta = new MenuItem("Pantalla completa");
     MenuItem opcionMinimizar = new MenuItem("Minimizar");
 
-    public BarraDeMenu(Stage stage)
-    {
+    public BarraDeMenu(Stage stage){
         Menu menuArchivo = nuevoMenuArchivo(stage);
         Menu menuVer = nuevoMenuVer(stage);
         Menu menuInformacion = nuevoMenuInformacion(stage);
@@ -25,32 +25,30 @@ public class BarraDeMenu extends MenuBar {
         this.getMenus().addAll(menuArchivo, menuVer, menuInformacion);
     }
 
-    private Menu nuevoMenuArchivo(Stage stage)
-    {
+    private Menu nuevoMenuArchivo(Stage stage){
         Menu menu = new Menu("Archivo");
         MenuItem opcionAbrir = new MenuItem("Abrir");
         MenuItem opcionSalir = new MenuItem("Salir");
         menu.getItems()
             .addAll(opcionAbrir, new SeparatorMenuItem(), opcionSalir);
 
-        EventHandler opcionSalirHandler = new OpcionSalirEventHandler(stage);
+        EventHandler<ActionEvent> opcionSalirHandler = new OpcionSalirEventHandler(stage);
         opcionSalir.setOnAction(opcionSalirHandler);
 
         return menu;
     }
 
-    private Menu nuevoMenuVer(Stage stage)
-    {
+    private Menu nuevoMenuVer(Stage stage){
         Menu menu = new Menu("Ver");
         menu.getItems().addAll(opcionPantallaCompleta, opcionMinimizar);
 
-        EventHandler opcionPantallaCompletaHandler =
+        EventHandler<ActionEvent> opcionPantallaCompletaHandler =
             new OpcionPantallaCompletaHandler(stage, opcionPantallaCompleta,
                 this);
         opcionPantallaCompleta.setOnAction(opcionPantallaCompletaHandler);
         opcionPantallaCompleta.setDisable(true);
 
-        EventHandler opcionMinimizarHandler =
+        EventHandler<ActionEvent> opcionMinimizarHandler =
             new OpcionMinimizarHandler(stage, opcionMinimizar, this);
         opcionMinimizar.setOnAction(opcionMinimizarHandler);
         opcionMinimizar.setDisable(false);
@@ -58,13 +56,12 @@ public class BarraDeMenu extends MenuBar {
         return menu;
     }
 
-    private Menu nuevoMenuInformacion(Stage stage)
-    {
+    private Menu nuevoMenuInformacion(Stage stage){
         Menu menu = new Menu("Informacion");
         MenuItem opcionInformacion = new MenuItem("Informacion");
         menu.getItems().addAll(opcionInformacion);
 
-        EventHandler opcionInformacionHandler =
+        EventHandler<ActionEvent> opcionInformacionHandler =
             new OpcionInformacionEventHandler(stage);
         opcionInformacion.setOnAction(opcionInformacionHandler);
 
