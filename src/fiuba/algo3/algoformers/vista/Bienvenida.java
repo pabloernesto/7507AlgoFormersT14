a1c2ec7aa572531c27e2090d90f386f5913f3b6e
pabloernesto <caussabon@gmail.com>
2016-06-20 19:48:01 -0300

    Refactor estetico

--- src/fiuba/algo3/algoformers/vista/Bienvenida.java
+++ src/fiuba/algo3/algoformers/vista/Bienvenida.java
@@ -19,58 +19,56 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class Bienvenida extends VBox{  
-    
+public class Bienvenida extends VBox
+{
+    Stage stage;
 
-        Stage stage;
+    public Bienvenida(Stage stage, Scene proximaEscena)
+    {
+        super();
+        this.stage = stage;
+        
+        this.setAlignment(Pos.CENTER_LEFT);
+        this.setSpacing(20);
+        this.setPadding(new Insets(480, 50, 50, 50));
+        
+        Image imagen = new Image("file:" + 
+            "src/fiuba/algo3/algoformers/vista/imagenes/intro/" +
+            "IntroPrincipal.jpg");
+        BackgroundImage imagenDeFondo =
+            new BackgroundImage(imagen, BackgroundRepeat.REPEAT,
+            BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
+            BackgroundSize.DEFAULT);
+        this.setBackground(new Background(imagenDeFondo));
 
-        public Bienvenida(Stage stage, Scene proximaEscena) {
+        Button botonEntrar = new Button();
+        botonEntrar.setText("Jugar!");
+        botonEntrar.setMinSize(200, 120);
+        botonEntrar.setFont(Font.font("Courier New",FontWeight.BOLD, 56));
+        
+        Button botonSalir = new Button();
+        botonSalir.setText("Salir :(");
+        botonSalir.setFont(Font.font("", 20));
 
-            super();
+        Button botonMaximizar= new Button();
+        botonMaximizar.setText("Maximizar Pantalla");
+        
+        Button botonMinimizar= new Button();
+        botonMinimizar.setText("Minimizar Pantalla");
 
-	        
-            this.stage = stage;
+        Label etiqueta = new Label();
+        etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 80));
 
+        etiqueta.setText("AlgoFormers");
+        etiqueta.setTextFill(Color.web("#66A7C5"));
 
-	        
-            this.setAlignment(Pos.CENTER_LEFT);
-            this.setSpacing(20);
-            this.setPadding(new Insets(480, 50, 50, 50));
-            Image imagen = new Image("file:src/fiuba/algo3/algoformers/vista/imagenes/intro/IntroPrincipal.jpg");
-            BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-            this.setBackground(new Background(imagenDeFondo));
-
-            Button botonEntrar = new Button();
-            botonEntrar.setText("Jugar!");
-            botonEntrar.setMinSize(200, 120);
-            botonEntrar.setFont(Font.font("Courier New",FontWeight.BOLD, 56));
-            
-            Button botonSalir = new Button();
-            botonSalir.setText("Salir :(");
-            botonSalir.setFont(Font.font("", 20));
-
-            
-
-            Button botonMaximizar= new Button();
-            botonMaximizar.setText("Maximizar Pantalla");
-            
-            Button botonMinimizar= new Button();
-            botonMinimizar.setText("Minimizar Pantalla");
-
-            Label etiqueta = new Label();
-            etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 80));
-
-            etiqueta.setText("AlgoFormers");
-            etiqueta.setTextFill(Color.web("#66A7C5"));
-
-            BotonEntrarEventHandler botonEntrarHandler = new BotonEntrarEventHandler(stage, proximaEscena);
-            botonEntrar.setOnAction(botonEntrarHandler);
-            
-            BotonSalirEventHandler botonSalirHandler = new BotonSalirEventHandler();
-            botonSalir.setOnAction(botonSalirHandler);
-            this.getChildren().addAll(etiqueta, botonEntrar,botonSalir);
-            
-        }
-
-    
+        BotonEntrarEventHandler botonEntrarHandler =
+            new BotonEntrarEventHandler(stage, proximaEscena);
+        botonEntrar.setOnAction(botonEntrarHandler);
+        
+        BotonSalirEventHandler botonSalirHandler = new BotonSalirEventHandler();
+        botonSalir.setOnAction(botonSalirHandler);
+        this.getChildren().addAll(etiqueta, botonEntrar,botonSalir);
+    }
 }
+
