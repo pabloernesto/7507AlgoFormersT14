bcbe9db900e1726b511675d915b379789fc5dba7
Farfanatico <niko.f2@gmail.com>
2016-06-20 16:40:27 -0300

    Boton aceptar event handler
    boton aceptar

--- src/fiuba/algo3/algoformers/vista/eventos/BotonAceptarEventHandler.java
+++ src/fiuba/algo3/algoformers/vista/eventos/BotonAceptarEventHandler.java
@@ -3,7 +3,6 @@ package fiuba.algo3.algoformers.vista.eventos;
 import java.util.List;
 
 import fiuba.algo3.algoformers.juego.Juego;
-import fiuba.algo3.algoformers.vista.ContenedorPrincipal;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -22,25 +21,21 @@ public class BotonAceptarEventHandler implements EventHandler<ActionEvent>{
     private Scene proximaEscena;
     private Stage stage;
     private Text jugador;
-    private ContenedorPrincipal contenedor;
 
-    public BotonAceptarEventHandler(TextField texto, Label etiqueta, Text jugador, List<String> nombresJugadores,
-    									Juego juego, Scene proxima, Stage stage, ContenedorPrincipal contenedor) {
+    public BotonAceptarEventHandler(TextField texto, Label etiqueta, Text jugador, List<String> nombresJugadores,Juego juego, Scene proxima, Stage stage) {
         this.texto = texto;
         this.etiqueta = etiqueta;
         this.nombresJugadores = nombresJugadores;
         this.juego = juego;
         this.stage = stage;
         this.jugador = jugador;
-        this.contenedor = contenedor;
         proximaEscena = proxima;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
 
-    	if (texto.getText().trim().equals(""))
-    	{
+    	if (texto.getText().trim().equals("")){
             etiqueta.setText("Debe ingresar un texto");
             etiqueta.setTextFill(Color.web("#FF0000")); // Rojo
         }
@@ -52,17 +47,15 @@ public class BotonAceptarEventHandler implements EventHandler<ActionEvent>{
         	nombresJugadores.add(texto.getText());
         	if (nombresJugadores.size() == 2){
         		juego.crearJugadores(nombresJugadores.get(0), nombresJugadores.get(1));
-        		juego.inicializar();
         		stage.setScene(proximaEscena);
         		stage.setFullScreenExitHint("");
                 stage.setFullScreen(true);
-                contenedor.iniciar();
         	}
-        	texto.setText("");
-        	texto.setFocusTraversable(false);
-        	texto.setPromptText("Ingrese su nombre");
-        	etiqueta.setText("");
-        	jugador.setText("Jugador Decepticons");
-        }
+			texto.setText("");
+			texto.setFocusTraversable(false);
+			texto.setPromptText("Ingrese su nombre");
+			etiqueta.setText("");
+			jugador.setText("Jugador Decepticons");
+			}
     }
 }
