9ae0b561c2002cda68864fc44714d53ba6494baa
Farfanatico <niko.f2@gmail.com>
2016-06-20 16:59:33 -0300

    Revert "Agregando barra de menu"
    This reverts commit 07d0cb8e1087d7ef7666b6347d625984a93949dd.

--- src/fiuba/algo3/algoformers/vista/BarraDeMenu.java
+++ src/fiuba/algo3/algoformers/vista/BarraDeMenu.java
@@ -9,51 +9,71 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.stage.Stage;
-
-
+import javafx.event.EventHandler;
 
 public class BarraDeMenu extends MenuBar {
-	
+
     MenuItem opcionPantallaCompleta = new MenuItem("Pantalla completa");
     MenuItem opcionMinimizar = new MenuItem("Minimizar");
 
-    public BarraDeMenu(Stage stage) {
-
-        Menu menuArchivo = new Menu("Archivo");
-        Menu menuVer = new Menu("Ver");
-        Menu menuInformacion = new Menu("Informacion");
-
-        MenuItem opcionSalir = new MenuItem("Salir");
-        MenuItem opcionAbrir = new MenuItem("Abrir");
-        MenuItem opcionInformacion = new MenuItem("Informacion");
-
-        OpcionSalirEventHandler opcionSalirHandler = new OpcionSalirEventHandler(stage);
-        opcionSalir.setOnAction(opcionSalirHandler);
-
-        OpcionInformacionEventHandler opcionInformacionHandler = new OpcionInformacionEventHandler(stage);
-        opcionInformacion.setOnAction(opcionInformacionHandler);
-
-        OpcionPantallaCompletaHandler opcionPantallaCompletaHandler = new OpcionPantallaCompletaHandler(stage, opcionPantallaCompleta,this);
-        opcionPantallaCompleta.setOnAction(opcionPantallaCompletaHandler);
-        
-
-        OpcionMinimizarHandler opcionMinimizarHandler = new OpcionMinimizarHandler(stage, opcionMinimizar,this);
-        opcionMinimizar.setOnAction(opcionMinimizarHandler);
-
-        opcionPantallaCompleta.setDisable(true);
-
-        opcionMinimizar.setDisable(false);
-        
-        menuArchivo.getItems().addAll(opcionAbrir, new SeparatorMenuItem(), opcionSalir);
-        menuInformacion.getItems().addAll(opcionInformacion);
-        menuVer.getItems().addAll(opcionPantallaCompleta,opcionMinimizar);
+    public BarraDeMenu(Stage stage)
+    {
+        Menu menuArchivo = nuevoMenuArchivo(stage);
+        Menu menuVer = nuevoMenuVer(stage);
+        Menu menuInformacion = nuevoMenuInformacion(stage);
 
         this.getMenus().addAll(menuArchivo, menuVer, menuInformacion);
+    }
+
+    private Menu nuevoMenuArchivo(Stage stage)
+    {
+        Menu menu = new Menu("Archivo");
+        MenuItem opcionAbrir = new MenuItem("Abrir");
+        MenuItem opcionSalir = new MenuItem("Salir");
+        menu.getItems()
+            .addAll(opcionAbrir, new SeparatorMenuItem(), opcionSalir);
+
+        EventHandler opcionSalirHandler = new OpcionSalirEventHandler(stage);
+        opcionSalir.setOnAction(opcionSalirHandler);
+
+        return menu;
+    }
+
+    private Menu nuevoMenuVer(Stage stage)
+    {
+        Menu menu = new Menu("Ver");
+        menu.getItems().addAll(opcionPantallaCompleta, opcionMinimizar);
+
+        EventHandler opcionPantallaCompletaHandler =
+            new OpcionPantallaCompletaHandler(stage, opcionPantallaCompleta,
+                this);
+        opcionPantallaCompleta.setOnAction(opcionPantallaCompletaHandler);
+        opcionPantallaCompleta.setDisable(true);
+
+        EventHandler opcionMinimizarHandler =
+            new OpcionMinimizarHandler(stage, opcionMinimizar, this);
+        opcionMinimizar.setOnAction(opcionMinimizarHandler);
+        opcionMinimizar.setDisable(false);
+
+        return menu;
+    }
+
+    private Menu nuevoMenuInformacion(Stage stage)
+    {
+        Menu menu = new Menu("Informacion");
+        MenuItem opcionInformacion = new MenuItem("Informacion");
+        menu.getItems().addAll(opcionInformacion);
+
+        EventHandler opcionInformacionHandler =
+            new OpcionInformacionEventHandler(stage);
+        opcionInformacion.setOnAction(opcionInformacionHandler);
+
+        return menu;
     }
 
     public void aplicacionMaximizada(Boolean estaMaximizada) {
         opcionPantallaCompleta.setDisable(estaMaximizada);
         opcionMinimizar.setDisable(!estaMaximizada);
     }
-
 }
+
