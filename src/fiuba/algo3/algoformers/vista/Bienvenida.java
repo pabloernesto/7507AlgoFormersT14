bdd2ec0c5d1c08da65c597f9f86fa7542291a1e4
Joaquin <joaquin.casal@yahoo.com.ar>
2016-06-22 20:52:01 -0300

    Agregado label a contenedor Bienvenida

--- src/fiuba/algo3/algoformers/vista/Bienvenida.java
+++ src/fiuba/algo3/algoformers/vista/Bienvenida.java
@@ -3,9 +3,11 @@ package fiuba.algo3.algoformers.vista;
 import fiuba.algo3.algoformers.vista.eventos.BotonEntrarEventHandler;
 import fiuba.algo3.algoformers.vista.eventos.BotonSalirEventHandler;
 import fiuba.algo3.algoformers.vista.eventos.OpcionSilenciarHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -14,6 +16,7 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -38,6 +41,13 @@ public class Bienvenida extends VBox
             BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT,
             BackgroundSize.DEFAULT);
         this.setBackground(new Background(imagenDeFondo));
+        
+        Label etiqueta = new Label();
+        etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 80));
+        etiqueta.setPadding(new Insets(40, 0, 0, 0));
+
+        etiqueta.setText("AlgoFormers");
+        etiqueta.setTextFill(Color.web("#66A7C5"));
 
         Button botonEntrar = new Button();
         botonEntrar.setText("Jugar!");
@@ -66,7 +76,7 @@ public class Bienvenida extends VBox
         OpcionSilenciarHandler botonSilenciarHandler = new OpcionSilenciarHandler(barraMenu, musica);
         botonSilenciar.setOnAction(botonSilenciarHandler);
         
-        this.getChildren().addAll(botonEntrar, botonSalir, botonSilenciar);
+        this.getChildren().addAll(etiqueta, botonEntrar, botonSalir, botonSilenciar);
     }
 }
 
