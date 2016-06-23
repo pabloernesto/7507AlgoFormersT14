b6e2a89708e7c9968a5cc4fe98492ba33eff597f
Joaquin <joaquin.casal@yahoo.com.ar>
2016-06-23 04:18:02 -0300

    Eliminados hashCode y equals innecesario
    Ya estan definido en la clase padre Efecto

--- src/fiuba/algo3/algoformers/escenario/efectos/EfectoConTurno.java
+++ src/fiuba/algo3/algoformers/escenario/efectos/EfectoConTurno.java
@@ -4,26 +4,6 @@ public abstract class EfectoConTurno extends Efecto {
 
 	protected int turnosRestantes;
 
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + turnosRestantes;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		return true;
-	}
-
 	public void restarTurnos() {
 		this.turnosRestantes--;
 	}
