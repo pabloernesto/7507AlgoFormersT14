c89cd8d2c62f64d603cb588ae14cf726ad667426
pabloernesto <caussabon@gmail.com>
2016-06-26 05:13:40 -0300

    Implementado crearError.
    Los errores salen por consola del juego.

--- src/fiuba/algo3/algoformers/vista/eventos/AccionAlgoformerEventHandler.java
+++ src/fiuba/algo3/algoformers/vista/eventos/AccionAlgoformerEventHandler.java
@@ -4,20 +4,16 @@ import fiuba.algo3.algoformers.juego.Juego;
 import fiuba.algo3.algoformers.vista.ContenedorPrincipal;
 import fiuba.algo3.algoformers.vista.ContenedorVictoria;
 import javafx.scene.Scene;
-//import javafx.scene.control.Alert;
-//import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public abstract class AccionAlgoformerEventHandler {
 
 	protected ContenedorPrincipal contenedorPrincipal;
 	
-	protected void crearError(String titulo, String encabezado, String mensaje){
-    	/*Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle(titulo);
-        alert.setHeaderText(encabezado);
-        alert.setContentText(mensaje);
-        alert.show();*/
+    void crearError(String titulo, String encabezado, String mensaje)
+    {
+        contenedorPrincipal.consola.setMensaje(titulo + ": " + encabezado);
+        contenedorPrincipal.consola.agregarMensaje("\t" + mensaje);
     }
 	
 	protected void chequearGanador(Juego juego){
