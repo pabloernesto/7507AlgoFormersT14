fb02f5fc5a2d1b684d817e4bba8b1dae9c711665
Farfanatico <niko.f2@gmail.com>
2016-06-28 00:43:41 -0300

    Cuando se transforman cambia la imagen de equipo
    Hue

--- src/fiuba/algo3/algoformers/vista/eventos/BotonTransformarseEventHandler.java
+++ src/fiuba/algo3/algoformers/vista/eventos/BotonTransformarseEventHandler.java
@@ -29,6 +29,7 @@ public class BotonTransformarseEventHandler implements EventHandler<ActionEvent>
             " se transformo!");
 		vistaTablero.actualizar(); //es importante que se dibuje despues de que haya terminado el turno
 		contenedorPrincipal.setBotoneraEleccion();
+		contenedorPrincipal.setImagenEquipo();
 		//Con este no hace falta, pero con el resto de los botones hay que chequear si hubo un ganador
 		//y si es asi, cambiar de escena, a la escena de ganador.
 	}
