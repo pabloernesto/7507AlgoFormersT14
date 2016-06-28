f47752e49f3724643df42128cb3a5c52776e05d9
Farfanatico <niko.f2@gmail.com>
2016-06-28 16:12:35 -0300

    Cambiadas flechas por fotos de flechas
    Para evitar bugs

--- src/fiuba/algo3/algoformers/escenario/Movimiento.java
+++ src/fiuba/algo3/algoformers/escenario/Movimiento.java
@@ -2,9 +2,9 @@ package fiuba.algo3.algoformers.escenario;
 
 public enum Movimiento {
 	
-	ARRIBA(0,-1,"↑"), ABAJO(0,1,"↓"), DERECHA(1,0,"→"), IZQUIERDA(-1,0,"←"),
-	ARRIBA_DERECHA(1,-1,"↗"), ARRIBA_IZQUIERDA(-1,-1,"↖"),
-	ABAJO_DERECHA(1,1,"↘"), ABAJO_IZQUIERDA(-1,1,"↙");
+	ARRIBA(0,-1,"norte"), ABAJO(0,1,"sur"), DERECHA(1,0,"este"), IZQUIERDA(-1,0,"oeste"),
+	ARRIBA_DERECHA(1,-1,"noreste"), ARRIBA_IZQUIERDA(-1,-1,"noroeste"),
+	ABAJO_DERECHA(1,1,"sureste"), ABAJO_IZQUIERDA(-1,1,"suroeste");
 	
 	private int movEnX;
 	private int movEnY;
